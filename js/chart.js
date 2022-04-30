e58bf581b84d38172aa6bcd7fde2338aaccb3d55
Mirjalol <miramirxomitov97@gmail.com>
2022-04-30 11:56:01 +0500

    added chart visitors

--- js/chart.js
+++ js/chart.js
@@ -285,6 +285,112 @@ function chart2(response) {
   chart.render();
 }
 
+// chart3 metrika visitors
+function chart3(response) {
+  var options = {
+    series: [
+      {
+        name: "Посетители",
+        data: [
+          { x: "01/10/2022", y: 10 },
+          { x: "02/10/2022", y: 20 },
+          { x: "03/10/2022", y: 30 },
+          { x: "04/10/2022", y: 85 },
+          { x: "05/10/2022", y: 47 },
+          { x: "06/10/2022", y: 65 },
+          { x: "07/10/2022", y: 18 },
+          { x: "08/10/2022", y: 4 },
+          { x: "09/10/2022", y: 74 },
+          { x: "10/10/2022", y: 50 },
+        ],
+      },
+    ],
+    chart: {
+      height: 350,
+      type: "line",
+      zoom: {
+        enabled: false,
+      },
+      fontFamily: "Roboto, sans-serif",
+      foreColor: "#333",
+    },
+    colors: ["#FBC100", "#219653", "#EB5757", "#2F80ED"],
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      width: 2,
+      curve: "straight",
+      // dashArray: [0, 8, 5],
+    },
+    // готов
+    title: {
+      text: "Посетители",
+      align: "left",
+    },
+    // готов
+    legend: {
+      // tooltipHoverFormatter: function (val, opts) {
+      //   return (
+      //     val +
+      //     " - " +
+      //     opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] +
+      //     ""
+      //   );
+      // },
+      position: "top",
+      horizontalAlign: "left",
+    },
+    // готов
+    markers: {
+      size: 0,
+      hover: {
+        sizeOffset: 6,
+      },
+    },
+    xaxis: {
+      labels: {
+        format: "dd.MM.yyyy",
+      },
+    },
+    // готов
+    tooltip: {
+      theme: false,
+      y: [
+        {
+          title: {
+            formatter: function (val) {
+              return val + " ";
+            },
+          },
+        },
+        {
+          title: {
+            formatter: function (val) {
+              return val;
+            },
+          },
+        },
+        {
+          title: {
+            formatter: function (val) {
+              return val;
+            },
+          },
+        },
+      ],
+    },
+    // готов
+    grid: {
+      borderColor: "#D3D3D3",
+      strokeDashArray: 3,
+    },
+  };
+
+  var chart = new ApexCharts(document.querySelector("#chart3"), options);
+  chart.render();
+}
+
 // pie-chart1
 function pieChart1(response) {
   var options = {
@@ -365,5 +471,6 @@ function pieChart2(response) {
 
 chart1();
 chart2();
+chart3();
 pieChart1();
 pieChart2();
